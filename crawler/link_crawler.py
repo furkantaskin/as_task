e408ded52058b7ff6028baba6cde8a58d238fd91
Furkan Taşkın <furkan.taskin@tuta.io>
2023-03-07 18:19:21 +0300

    Add loading bar and ensure folder exist - Add loading bar to track the progress of files downloaded - Ensure folder for assets crawled from website exist. If not, then create the folder

--- crawler/link_crawler.py
+++ crawler/link_crawler.py
@@ -1,4 +1,5 @@
 import cloudscraper
+import os
 
 scraper = cloudscraper.create_scraper()
 
@@ -12,13 +13,16 @@ def download_assets(url_list: list) -> bool:
     """
 
     print("\033[0;35m Downloading assets... \033[0m")
-    for url in url_list:
+    for i, url in enumerate(url_list, start=1):
         try:
             if "classcentral.com" in url:
-                print("\033[0;93m Downloading: \033[0m", url)
+                square = "#" * i
+                underscore = "_" * (len(url_list) - i)
+                print("\r", end=f"\033[0;93m {square+underscore} Downloading: \033[0m {url}")
                 file_name = url.split("/")[-1]
                 file_folder: str = url.split("/")[3]
-                with open(f"../res/{file_folder}/{file_name}", "wb") as f:
+                os.makedirs(f"res/{file_folder}", exist_ok=True)
+                with open(f"res/{file_folder}/{file_name}", "wb") as f:
                     f.write(scraper.get(url).content)
                     f.close()
         except IndexError as e:
