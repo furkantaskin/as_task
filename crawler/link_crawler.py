8983efa292d342ba938b67f0457cf44143371f50
Furkan Taşkın <furkan.taskin@tuta.io>
2023-03-07 22:13:51 +0300

    Add information for functions

--- crawler/link_crawler.py
+++ crawler/link_crawler.py
@@ -7,12 +7,18 @@ scraper = cloudscraper.create_scraper()
 def download_assets(url_list: list) -> bool:
     """
 
+    It will download the assets from the list of urls provided. It will create a folder for each domain name extracted
+    from the url. For example if url is like http://www.example.com/assets/assetname.js then it will create a folder
+    named assets inside the res folder and save the file as assetname.js in it.
+
     :param url_list: List of urls to download
     :return: True if all urls were downloaded successfully
 
     """
 
     print("\033[0;35m Downloading assets... \033[0m")
+    # For each url in the list, download the file and save it in the res folder.
+    # Show progress for to detect how many files are downloaded.
     for i, url in enumerate(url_list, start=1):
         try:
             if "classcentral.com" in url:
