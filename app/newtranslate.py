fff1e99d32cd6c6be0370a023df937a19e837e45
Furkan Taşkın <furkan.taskin@tuta.io>
2023-03-09 23:29:48 +0300

    Create functions for reading and writing operations for json file in l10n

--- app/newtranslate.py
+++ app/newtranslate.py
@@ -2,27 +2,67 @@ from bs4 import BeautifulSoup
 import translators as ts
 import translators.server as tss
 import time
+import json
+import os
+
+string_list_hi = []
+custom_list = ["../docs-copy/index.html", "../docs-copy/contact.html"]
+
+
+def write_to_json(data=None):
+    if data is None:
+        data = {}
+    with open("../l10n/en-hi.json", "r", encoding="utf-8") as f:
+        json_data = json.load(f)
+        f.close()
+    merged_data = {**json_data, **data}
+
+    with open("../l10n/en-hi.json", "w", encoding="utf-8") as f:
+        json.dump(merged_data, f, indent=4, ensure_ascii=False)
+        f.close()
+
+
+def read_from_json() -> dict:
+    with open("../l10n/en-hi.json", "r", encoding="utf-8") as f:
+        json_data = json.load(f)
+        f.close()
+    return json_data
+
+
+def convert_file(arg_file="../docs-copy/index.html"):
+    en_text = read_from_json().keys()
+    with open(arg_file, 'r', encoding="utf-8") as html_file:
+        html = html_file.read()
+        html_file.close()
+
+    soup = BeautifulSoup(html, 'html.parser')
+    for inner_text in [*set(soup.get_text().split("\n"))]:
+        if inner_text in en_text:
+            continue
+        else:
+            write_to_json({inner_text: tss.google(inner_text, to_language="hi")})
+            print(f"Translated {inner_text} to {tss.google(inner_text, to_language='hi')} and added to the JSON")
+    with open(arg_file, 'w+', encoding="utf-8") as html_file:
+
+        html_file.write(str(soup))
+        html_file.close()
 
 # Load the HTML file
-with open('../docs-copy/index.html', 'r') as f:
-    html = f.read()
+
 
 # Parse the HTML file
-soup = BeautifulSoup(html, 'html.parser')
 
-# Initialize the translator
-
-# Find all the elements located in the body
+# print(read_from_json())
 
 
-string_list = [x.strip(" ") for x in [*set(soup.get_text().split("\n"))] if x.replace(" ", "")]
-string_list_hi = []
-for s in string_list:
-    print("\n\033[0;96m Translating \033[0m", s)
-    x = tss.google(s, to_language="hi")
-    print("\033[0;96m Translated \033[0m", x)
-    string_list_hi.append(x)
-    time.sleep(2)
 
-print(len(string_list_hi), len(string_list))
+# string_list = [x.strip(" ") for x in [*set(soup.get_text().split("\n"))] if x.replace(" ", "")]
+
+#
+#
+#
+#
+# json.dump(dict.fromkeys(string_list), local_file, indent=4, ensure_ascii=False)
+#
+# local_file.close()
 
